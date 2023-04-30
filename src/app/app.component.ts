89bdd8a712dacb3fd5d2f8ba5a5fe19e6640dbef
Jean Medeiros <jeanmail@gmail.com>
2023-04-30 11:01:48 -0300

    Mudando o texto de retorno da página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+//  template: `<div>Hello {{value}}</div>`,
+  template: `<div>{{message}}</div>`,
 })
 export class AppComponent {
-  value = 'World';
+//  value = 'World';
+message = '';
+
+constructor(private http: HttpClient) {
+  this.http.get('/api/message')
+    .subscribe((resp: any) => this.message = resp.text);
+}
 }
