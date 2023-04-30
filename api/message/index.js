3f9989b5394541d3bd4bf1d9207577b306868b96
Jean Medeiros <jeanmail@gmail.com>
2023-04-30 11:09:33 -0300

    Voltando ao arquivo index.js, vamos adicionar o seguinte código:
    ```js

--- api/message/index.js
+++ api/message/index.js
@@ -1,18 +1,18 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    // context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Olá, " + name + ". Esta tudo funcionando perfeitamente bem aqui."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // const name = (req.query.name || (req.body && req.body.name));
+    // const responseMessage = name
+    //     ? "Olá, " + name + ". Esta tudo funcionando perfeitamente bem aqui."
+    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    // context.res = {
+    //     // status: 200, /* Defaults to 200 */
+    //     body: responseMessage
+    // };
 
-    // context.res.json({
-    //     text: "Hello from the API"
-    // });
+    context.res.json({
+        text: "Hello from the API"
+    });
 
 }
